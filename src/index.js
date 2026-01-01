a0a2765b8e058cbcbb390a8feac7a0be2ac4195b
LAPTOP-Q0HKJNMT\hp <zawwinthant317@gmail.com>
2026-01-01 15:48:49 +0630

    fix: remove unused imports to satisfy ESLint for CI build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import router from "./router/index.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
